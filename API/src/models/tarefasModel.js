9262a128a2e2891a4a626877b4b3be71326e7bcd
Mariana Kanashiro <m.kanashiro@iesb.edu.br>
2025-11-06 10:23:44 -0300

    alterando tarefasModel, criando tarefasController com as funções criar, listar, buscar, exibir, atualizar e remover, e criando rotas POST para criação de nova tarefa e GET para buscar todas as tarefas

--- API/src/models/tarefasModel.js
+++ API/src/models/tarefasModel.js
@@ -2,7 +2,13 @@ const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
     email: {type: String, required: [true, 'Email é obrigatorio'], unique: true}, senha: {type: String, required: true}, 
+    nome: {
+        type: String, 
+        required: [true, 'Nome da tarefa é obrigatório.'],
+        trim: true,
+        minLength: [3, 'Nome da tarefa deve ter pelo menos 3 caracteres.']
+    },
     concluida: Boolean
 });
 
-module.exports = mongoose.model('Tarefa', schema)
+module.exports = mongoose.model('Tarefa', schema);
