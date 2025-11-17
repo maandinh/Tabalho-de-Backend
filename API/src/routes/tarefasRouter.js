86e656c8863ed707550cb1e8cd2f6916757dd170
FelipeGripp <souzagrippfelipe@gmail.com>
2025-11-17 19:11:58 -0300

    Middleware para validar JWT e proteger rotas de tarefas

--- API/src/routes/tarefasRouter.js
+++ API/src/routes/tarefasRouter.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const controller = require('../controllers/tarefasController');
+const { verificarToken } = require("../middleware/authMiddleware");
 
 const router = express.Router();
+
+router.use(verificarToken);
 
 router.post('/', controller.criar);
 
